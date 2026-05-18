734b75d6e2519ae5cdad8c8c14f67c2f5a1f201a
elizabetha-tha <5061488@clases.edu.sv>
2026-05-18 17:00:26 -0600

    este es el archivo principal
    este archivo contiene todos los ejercicios

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Menu</title>
+</head>
+<body>
+
+    <h1>Ejercicios de HTML a PHP</h1>
+
+    <ul>
+        <li>Ejercicio 1: <a href="ejercicio1.html">HTML</a> | <a href="ejercicio1.php">PHP</a></li>
+        <li>Ejercicio 2: <a href="ejercicio2.html">HTML</a> | <a href="ejercicio2.php">PHP</a></li>
+        <li>Ejercicio 3: <a href="ejercicio3.html">HTML</a> | <a href="ejercicio3.php">PHP</a></li>
+        <li>Ejercicio 4: <a href="ejercicio4.html">HTML</a> | <a href="ejercicio4.php">PHP</a></li>
+        <li>Ejercicio 5: <a href="ejercicio5.html">HTML</a> | <a href="ejercicio5.php">PHP</a></li>
+        <li>Ejercicio 6: <a href="ejercicio6.html">HTML</a> | <a href="ejercicio6.php">PHP</a></li>
+        <li>Ejercicio 7: <a href="ejercicio7.html">HTML</a> | <a href="ejercicio7.php">PHP</a></li>
+        <li>Ejercicio 8: <a href="ejercicio8.html">HTML</a> | <a href="ejercicio8.php">PHP</a></li>
+        <li>Ejercicio 9: <a href="ejercicio9.html">HTML</a> | <a href="ejercicio9.php">PHP</a></li>
+        <li>Ejercicio 10: <a href="ejercicio10.html">HTML</a> | <a href="ejercicio10.php">PHP</a></li>
+        <li>Ejercicio 11: <a href="ejercicio11.html">HTML</a> | <a href="ejercicio11.php">PHP</a></li>
+        <li>Ejercicio 12: <a href="ejercicio12.html">HTML</a> | <a href="ejercicio12.php">PHP</a></li>
+        <li>Ejercicio 13: <a href="ejercicio13.html">HTML</a> | <a href="ejercicio13.php">PHP</a></li>
+        <li>Ejercicio 14: <a href="ejercicio14.html">HTML</a> | <a href="ejercicio14.php">PHP</a></li>
+        <li>Ejercicio 15: <a href="ejercicio15.html">HTML</a> | <a href="ejercicio15.php">PHP</a></li>
+        <li>Ejercicio 16: <a href="ejercicio16.html">HTML</a> | <a href="ejercicio16.php">PHP</a></li>
+        <li>Ejercicio 17: <a href="ejercicio17.html">HTML</a> | <a href="ejercicio17.php">PHP</a></li>
+        <li>Ejercicio 18: <a href="ejercicio18.html">HTML</a> | <a href="ejercicio18.php">PHP</a></li>
+        <li>Ejercicio 19: <a href="ejercicio19.html">HTML</a> | <a href="ejercicio19.php">PHP</a></li>
+        <li>Ejercicio 20: <a href="ejercicio20.html">HTML</a> | <a href="ejercicio20.php">PHP</a></li>
+        <li>Ejercicio 21: <a href="ejercicio21.html">HTML</a> | <a href="ejercicio21.php">PHP</a></li>
+        <li>Ejercicio 22: <a href="ejercicio22.html">HTML</a> | <a href="ejercicio22.php">PHP</a></li>
+        <li>Ejercicio 23: <a href="ejercicio23.html">HTML</a> | <a href="ejercicio23.php">PHP</a></li>
+        <li>Ejercicio 24: <a href="ejercicio24.html">HTML</a> | <a href="ejercicio24.php">PHP</a></li>
+        <li>Ejercicio 25: <a href="ejercicio25.html">HTML</a> | <a href="ejercicio25.php">PHP</a></li>
+    </ul>
+
+</body>
+</html>
